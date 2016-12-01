cf13bc34179100f65bfc14b23ab8e14b5b1eab92
jebiba95 <jesusbintaned95@gmail.com>
2016-12-01 11:26:33 +0100

    Change SearchController.java to extend the query

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class SearchController {
@@ -22,7 +25,40 @@ public class SearchController {
 
     @RequestMapping(value="/search")
     @ResponseBody
-    public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+    public Object search(@RequestParam("q") String query) {
+        
+        //Map with the headers of the CamelRequest
+        Map headers = new HashMap();
+
+        //String query "kewword max:n" -> Now separate the keyword (keyword) of count (max:n)
+        if (query.contains(" max:")) {
+
+             /*
+             * SPLIT, the query contains " max:n" then:
+             * 
+             * Headers: {
+             *    CamelTwitterKeywords: querySplit[0],
+             *    CamelTwitterCount: querySplit[1]
+             * }
+             */
+            String[] queryParts = query.split(" max:");
+
+            //Put the specifics headers into Map of CamelRequest
+            headers.put("CamelTwitterKeywords", queryParts[0]);
+            headers.put("CamelTwitterCount", queryParts[1]);
+        } else {
+            
+            /*
+             * NOT SPLIT, the query not contains " max:n" then:
+             * 
+             * Headers: {
+             *    CamelTwitterKeywords: query
+             *
+             * }
+             */
+            headers.put("CamelTwitterKeywords", query);       
+        }
+
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
     }
 }
